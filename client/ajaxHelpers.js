81aa1091d3c1c0a9fd69a9ea51cb25e368918cca
Brain <batayolageorgebrain@gmail.com>
2023-01-11 11:12:53 -0600

    first commit

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -1,5 +1,5 @@
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'COHORT-NAME';
+const cohortName = '2211-ftb-et-web-am';
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
